7b7f9e78809a405d7b7fee009a774489be8066c6
Pawel Domaradzki <gamingprodukcje@gmail.com>
2023-10-01 18:55:02 +0200

    add sample welcome img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import Search from "@/components/Search";
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="wrapper">
       <div className="container">
+        <Image
+          src="https://i.imgur.com/4QMunse.png"
+          alt="champ"
+          width={350}
+          height={277}
+        />
         <Search />
       </div>
     </main>
